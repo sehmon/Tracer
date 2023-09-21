06ded744db5133c0b078c7bdd24f25a62f022bee
Sehmon Burnam <sehmon3@gmail.com>
2023-09-21 12:09:30 +0000

    skip check every other frame count for a bit better latency

--- public/js/tracer.js
+++ public/js/tracer.js
@@ -50,6 +50,31 @@ function sendMouseUpdateToServer() {
   })
 }
 
+let infoBtn, infoDiv, infoTitle, infoP, infoLink;
+function setupHTMLElements() {
+  createCanvas(windowWidth, windowHeight);
+
+  infoBtn = createButton("?");
+  infoBtn.position(windowWidth - 40, 20);
+  infoBtn.mousePressed(togglePopup);
+
+  infoDiv = createDiv();
+  infoDiv.position(width / 2 - 150, height / 2 - 100);
+  infoDiv.style('background', 'white');
+  infoDiv.style('text-align', 'center');
+  infoDiv.style('padding', '20px');
+  infoDiv.style('border', '1px solid black');
+  infoDiv.hide();
+
+  infoTitle = createElement('h1', 'Connections');
+  infoTitle.parent(infoDiv);
+
+  infoParagraph = createP(projectText);
+  infoParagraph.style('text-align', 'left');
+  infoParagraph.parent(infoDiv);
+
+}
+
 function drawUI() {
   noStroke();
   fill(0);
@@ -57,19 +82,11 @@ function drawUI() {
     `${user_count} active user` :
     `${user_count} active users`;
   textAlign(RIGHT);
-  text(count_string, windowWidth-40, windowHeight-20);
+  text(count_string, windowWidth-20, windowHeight-20);
   textAlign(LEFT);
   textSize(32);
   text("Connections", 20, 40);
   textSize(12);
-}
-
-function drawInfoPopup() {
-  fill(255);
-  rect(width / 2 - 150, height / 2 - 100, 300, 200);
-  fill(0);
-  textAlign(CENTER, CENTER);
-  text('This is some informational text.', width / 2, height / 2);
 }
 
 function drawServerGraphAndUsers() {
@@ -102,10 +119,12 @@ function drawServerGraphAndUsers() {
     textAlign(LEFT);
     text(users[u].screenName, x_pos, y_pos);
     // TODO: pre-build multi-line string to keep for loop out of draw()
+    /**
     for(let i=0; i<users[u].path.length; i++){
       fill(140);
       text(users[u].path[i], x_pos + 10, y_pos + ((i+1)*12))
     }
+    **/
   }
 }
 
@@ -116,33 +135,15 @@ let projectText = `In the pursuit of frictionless technology we have abstracted 
 Through this project, the visitor experiences shared presence while replicating the underlying network topology that makes the experience possible. By interacting with the screen, the user explores ideas of connection through modeling the physical connection of digital devices.<br><br>
 
 This project takes inspiration from various interactive web experiences, specifically the work of pioneers in this space like Myron Kruger VIDEOPLACE. Similar to Myron, this project attempts to explore how technology can be a medium for connection by replicating a user's identity and modeling shared space in a way that feels tangible.`
-let infoBtn, infoDiv, infoTitle, infoP, infoLink;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  infoBtn = createButton("?");
-  infoBtn.position(windowWidth - 60, 40);
-  infoBtn.mousePressed(togglePopup);
-
-  infoDiv = createDiv();
-  infoDiv.position(width / 2 - 150, height / 2 - 100);
-  infoDiv.style('background', 'white');
-  infoDiv.style('text-align', 'center');
-  infoDiv.style('padding', '20px');
-  infoDiv.style('border', '1px solid black');
-  infoDiv.hide();
-
-  infoTitle = createElement('h1', 'Connections');
-  infoTitle.parent(infoDiv);
-
-  infoParagraph = createP(projectText);
-  infoParagraph.style('text-align', 'left');
-  infoParagraph.parent(infoDiv);
+  setupHTMLElements();
+  sendMouseUpdateToServer();
 }
 
 function draw() {
-  if (pmouseX !== mouseX || pmouseY !== mouseY) {
+  if (pmouseX !== mouseX || pmouseY !== mouseY && frameCount % 2 == 0) {
     sendMouseUpdateToServer();
   }
 
