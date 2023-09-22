2b152227ff590793c4bec19f6665142ab832a0dc
Sehmon Burnam <sehmon3@gmail.com>
2023-09-22 18:02:26 +0000

    NetworkGraphManager refactor

--- src/index.js
+++ src/index.js
@@ -19,15 +19,15 @@ const detector = new DeviceDetector({
   deviceAliasCode: false,
 });
 const userManager = new UserManager();
+const networkGraphManager = new NetworkGraphManager();
 
 
 // Set up Network Graph and add server as root
-let server_graph = new NetworkGraph();
 getHostIPAddress()
   .then(ipAddress => {
     // This is bad, create a function
     userManager.users['SERVER'].screenName = `New York City - ${ipAddress}`; 
-    server_graph.root = new GraphNode("Server", ipAddress, 'root-server-node')})
+    networkGraphManager.initializeServerGraph(ipAddress);})
   .catch(error => {console.error(`Failed to get Server IP address: ${error.message}`)});
 
 // Server Routes
@@ -40,7 +40,7 @@ app.get('/visualize', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/graph.html'));
 });
 
-app.get('/test-graph', (req, res) => { res.send(server_graph)});
+app.get('/test-graph', (req, res) => { res.send(networkGraphManager.networkGraph)});
 
 io.on('connection', async (socket) => {
   // Initialize individual user's info
@@ -53,44 +53,7 @@ io.on('connection', async (socket) => {
   // send id back to user
   socket.emit('yourID', id);
 
-  // Create graph node for the new user
-  let user_node = new GraphNode(screenName, userIP, deviceType);
-  let prev = server_graph.root;
-  server_graph.addNode(user_node);
-  prev.addChild(user_node);
-
-  // Iterate through traceroute command
-  const { spawn } = require('child_process');
-  const child = spawn('traceroute', ['-q 1', userIP]);
-  // Following regex gets each servername/ip pair from the traceroute output
-  const regex = /(\S+)\s+\((\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b)\)|(\*)/g;
-
-
-  child.stdout.setEncoding('utf8');
-  child.stdout.on('data', (chunk) => {
-    for (const match of chunk.toString().matchAll(regex)) {
-      if (match[3]) { // If the third capture group (asterisk) is matched
-        // console.log(`Asterisk: ${match[3]}`);
-      } else {
-        const serverName = match[1];
-        const ipAddress = match[2];
-
-        if(ipAddress == user_node.ip){
-          continue;
-        }
-
-        let n = new GraphNode(serverName, ipAddress, 'intermediate-node');
-        server_graph.addIntermediateNode(prev, user_node, n);
-        prev = n;
-
-        path.push(`${serverName} (${ipAddress})`);
-      }
-    }
-  });
-
-  child.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-  });
+  path = await networkGraphManager.traceIPRoute(userManager.getUser(id));
 
   // Grab location information from IP Address
   fetch(`http://ip-api.com/json/${userIP}`)
@@ -107,14 +70,12 @@ io.on('connection', async (socket) => {
   socket.on('setUserAgent', (agent) => {
     const result = detector.detect(agent);
     deviceType = result.device.type;
-    console.log(deviceType);
     userManager.setDeviceType(id, deviceType);
   });
 
   socket.on('disconnect', () => {
     console.log('user disconnected');
     userManager.removeUser(id);
-    console.log(`${id} deleted`)
   });
 
   // When receiveing a Mouse Update from a device, update position and broadcast to the rest of connected users
@@ -123,7 +84,7 @@ io.on('connection', async (socket) => {
       { x: mouseData.x, y: mouseData.y, screenWidth: mouseData.screenWidth, screenHeight: mouseData.screenHeight, screenName: screenName, deviceType: deviceType, path: path}
     );
     io.emit("userUpdate", { 'users': userManager.users, 'count': userManager.userCount } );
-    io.emit("serverGraphUpdate", server_graph);
+    io.emit("serverGraphUpdate", networkGraphManager.networkGraph);
   })
 });
 
--- src/managers/NetworkGraphManager.js
+++ src/managers/NetworkGraphManager.js
@@ -1,4 +1,64 @@
+const NetworkGraph = require('../models/NetworkGraph');
+const GraphNode = require('../models/GraphNode');
+const { spawn } = require('child_process')
+
 class NetworkGraphManager {
+  constructor() {
+    this.networkGraph = new NetworkGraph();
+  }
+
+  // You should be able to do this in the NetworkGraph class, not here
+  initializeServerGraph(ipAddress) {
+    this.networkGraph.root = new GraphNode("Server", ipAddress, 'root-server-node');
+  }
+
+  async traceIPRoute(user) {
+    // Create the new GraphNode and prep for the traceroute
+    let userNode = new GraphNode(user.screenName, user.ip, user.deviceType);
+    let path = [];
+    let prev = this.networkGraph.root;
+    path.push(`${this.networkGraph.root.name} (${this.networkGraph.root.ip})`);
+    this.networkGraph.addNode(userNode);
+    prev.addChild(userNode);
+
+    console.log("Starting traceroute");
+    const child = spawn('traceroute', ['-q', '1', user.ip]);
+
+    child.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+    });
+
+    // Following regex gets each servername/ip pair from the traceroute output
+    const regex = /(\S+)\s+\((\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b)\)|(\*)/g;
+
+    child.stdout.setEncoding('utf8');
+    child.stdout.on('data', (chunk) => {
+      for (const match of chunk.toString().matchAll(regex)) {
+        if (match[3]) { // If the third capture group (asterisk) is matched
+          // console.log(`Asterisk: ${match[3]}`);
+        } else {
+          const serverName = match[1];
+          const ipAddress = match[2];
+
+          if(ipAddress == userNode.ip){
+            continue;
+          }
+
+          let n = new GraphNode(serverName, ipAddress, 'intermediate-node');
+          this.networkGraph.addIntermediateNode(prev, userNode, n);
+          prev = n;
+
+          path.push(`${serverName} (${ipAddress})`);
+        }
+      }
+    });
+
+    child.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+
+    return path;
+  }
 }
 
 module.exports = NetworkGraphManager;
